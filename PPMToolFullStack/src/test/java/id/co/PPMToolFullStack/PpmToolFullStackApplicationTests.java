6e9a5fa30555988996ecbf4ed2b7ea08319ad932
suhartonoyang <suhartonoyang@gmail.com>
2020-05-27 09:53:38 +0700

    First Commit

---
+++ PPMToolFullStack/src/test/java/id/co/PPMToolFullStack/PpmToolFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.PPMToolFullStack;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmToolFullStackApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
