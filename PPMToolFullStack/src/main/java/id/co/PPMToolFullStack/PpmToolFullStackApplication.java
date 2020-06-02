7e6e0e4e2561809c88cd7250e4a4aa176a1a9d2d
Suhartono <suhartonoyang@gmail.com>
2020-06-02 16:22:18 +0700

    Setup for eclipse

--- PPMToolFullStack/src/main/java/id/co/PPMToolFullStack/PpmToolFullStackApplication.java
+++ PPMToolFullStack/src/main/java/id/co/PPMToolFullStack/PpmToolFullStackApplication.java
@@ -1,13 +1,44 @@
 package id.co.PPMToolFullStack;
 
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
-public class PpmToolFullStackApplication {
+public class PpmToolFullStackApplication extends SpringBootServletInitializer {
+
+//	public static void main(String[] args) {
+//		SpringApplication.run(PpmToolFullStackApplication.class, args);
+//	}
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return super.configure(builder);
+//	}
+//	
+//	
 
 	public static void main(String[] args) {
-		SpringApplication.run(PpmToolFullStackApplication.class, args);
+		SpringApplicationBuilder springApplicationBuilder = (SpringApplicationBuilder) new SpringApplicationBuilder(
+				PpmToolFullStackApplication.class).sources(PpmToolFullStackApplication.class)
+						.properties(getProperties()).run(args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(PpmToolFullStackApplication.class).properties(getProperties());
+	}
+
+	static Properties getProperties() {
+		Properties properties = new Properties();
+		properties.put("spring.config.location", "classspath:PPMTool");
+		return properties;
 	}
 
 }
